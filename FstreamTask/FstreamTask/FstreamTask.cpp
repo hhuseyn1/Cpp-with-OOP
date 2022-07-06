f6a5dbf7e1b1973d407937b03d88951a4a28ea6f
Huseyn <88426345+hhuseyn1@users.noreply.github.com>
2022-07-06 19:24:21 +0400

    Fstream
    Fstream Task

---
+++ FstreamTask/FstreamTask/FstreamTask.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+#include<fstream>
+#include<vector>
+#include<string>
+#include "Applier.h"
+#include "Company.h"
+
+
+
+int main() {
+	Applier* cv1 = new Applier("Huseyn", 18, "IT", "Empty1");
+	Applier* cv2 = new Applier("Huseyn", 18, "Design", "Empty2");
+
+
+	Company company;
+	company.hireUp(cv1);
+	company.hireUp(cv2);
+
+	company.writeToFile("company.txt");
+
+	company.showAllCV();
+
+	company.readToFile("company.txt");
+
+}
